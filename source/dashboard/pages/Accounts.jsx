65032deb7da52b2f16fff1c812ee319d5360d38d
Tanin Ahmed <taninakond@gmail.com>
2025-05-21 00:33:41 +0600

    use i18n in Accounts page

--- source/dashboard/pages/Accounts.jsx
+++ source/dashboard/pages/Accounts.jsx
@@ -1,5 +1,6 @@
 import Section from '../components/Section'
 import Switch from '../components/Switch'
+import __ from '@wordpress/i18n'
 
 const Accounts = () => {
     const handleChange = (value) => {
@@ -7,9 +8,9 @@ const Accounts = () => {
 	}
     return (
         <div className="bdp-content-box">
-            <Section title="Authorization Type">
-                <Switch id="oneClickAuthorization" label="One Click Authorization" onChange={handleChange} />
-                <div className="bdp-section-description">If you want to use one click authorization, you can enable this option</div>
+            <Section title={__('Authorization Type', 'cf7-google-sheet')}>
+                <Switch id="oneClickAuthorization" label={__('One Click Authorization', 'cf7-google-sheet')} onChange={handleChange} />
+                <div className="bdp-section-description">{__('If you want to use one click authorization, you can enable this option.', 'cf7-google-sheet')}</div>
             </Section>
         </div>
     )
