4e0776d4c88060894eac5cc3b32bb40edd11578b
Tanin Ahmed <taninakond@gmail.com>
2025-06-14 17:22:47 +0600

    optimize select component

--- source/dashboard/components/Select.jsx
+++ source/dashboard/components/Select.jsx
@@ -1,23 +1,29 @@
 import { useState } from '@wordpress/element';
 
-const Select = ({ id, label, value, options, onChange }) => {
-
+const SelectOptions = ({ id, options, value, onChange }) => {
     const [_value, setValue] = useState(value || '');
 
     const handleChange = (value) => {
         setValue(value);
-        onChange({value, target: id});
+        onChange({ value, target: id });
     }
+
+    return (
+        <select value={_value} onChange={(e) => handleChange(e.target.value)} className="bdp-select" id={id}>
+            <option value="">Select One</option>
+            {options.map((option) => (
+                <option className="bdp-select-option" key={option.value} value={option.value}>{option.label}</option>
+            ))}
+        </select>
+    )
+}
+
+const Select = ({ id, label, value, options, onChange }) => {
 
     return (
         <div className="bdp-select-wrapper">
             <label className="bdp-select-label" htmlFor={id}>{label}</label>
-            <select value={_value} onChange={(e) => handleChange(e.target.value)} className="bdp-select" id={id}>
-                <option value="">Select One</option>
-                {options.map((option) => (
-                    <option className="bdp-select-option" key={option.value} value={option.value}>{option.label}</option>
-                ))}
-            </select>
+            <SelectOptions id={id} options={options} value={value} onChange={onChange} />
         </div>
     )
 }
